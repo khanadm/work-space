3205b197ed8dc7c403841122ab5705d6bb100532
root <root@localhost.localdomain>
2023-06-21 18:32:07 +0530

    added files

---
+++ php_application/backend/init.sql
@@ -0,0 +1,9 @@
+GRANT ALL PRIVILEGES ON ongraph.* TO 'ongraph'@'%';
+USE ongraph;
+
+CREATE TABLE users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+); 
